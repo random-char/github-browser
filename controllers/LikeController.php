0f566fb473c5189c0c85dca12969601a1488032b
random-char <mailbox2bfq@gmail.com>
2017-01-18 18:40:25 +0200

    ajax request handling controller

---
+++ controllers/LikeController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\RepoLikes;
+use app\models\UserLikes;
+use Yii;
+use yii\web\BadRequestHttpException;
+use yii\web\Controller;
+
+class LikeController extends Controller
+{
+    public function actionVote($type, $name)
+    {
+        file_put_contents('/tmp/q', $type . "\n" . $name);
+        if ($type == 'user') {
+            $like = UserLikes::findOne(['username' => $name]);
+            if (empty($like)) {
+                $like = new UserLikes();
+                $like->username = $name;
+                $like->value = 1;
+                $like->save();
+            } else {
+                $like->value = ($like->value + 1) % 2;
+                $like->save();
+            }
+        } elseif ($type == 'repo') {
+            $like = RepoLikes::findOne(['repo_fullname' => $name]);
+            if (empty($like)) {
+                $like = new RepoLikes();
+                $like->repo_fullname = $name;
+                $like->value = 1;
+                $like->save();
+            } else {
+                $like->value = ($like->value + 1) % 2;
+                $like->save();
+            }
+        } else {
+            throw new BadRequestHttpException('Wrong type!');
+        }
+    }
+}
