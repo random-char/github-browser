9e08f54ddd05c4ef804628193e2fa94c12175e16
random-char <mailbox2bfq@gmail.com>
2017-01-18 18:22:36 +0200

    ajax like calls

---
+++ web/js/script.js
@@ -0,0 +1,14 @@
+function vote(element, type) {
+    console.log(element);
+    $.ajax({
+        url: "/like/" + type + "/" + $(element).data("name"),
+        success: function(){
+        if ($(element).hasClass("btn-success")) {
+            $(element).removeClass("btn-success").addClass("btn-danger");
+            $(element).text('UnLike');
+        } else {
+            $(element).removeClass("btn-danger").addClass("btn-success");
+            $(element).text('Like');
+        }
+    }});
+}
